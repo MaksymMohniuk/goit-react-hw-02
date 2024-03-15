9c06fafce13e35d03d7ae6bc91066a210ec16d25
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-03-16 00:14:31 +0200

    components-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 // import { useState } from 'react'
-import Description from "./assets/Description/Description.jsx";
+import Description from "./components/Description/Description.jsx";
+import Options from "./components/Options/Options.jsx";
+import Feedback from "./components/Feedback/Feedback.jsx";
 
 function App() {
   return (
     <>
       <Description />
+      <Options />
+      <Feedback />
     </>
   );
 }
