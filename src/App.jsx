47af6b7c059394ead2eb07dbe915d2244157a0eb
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-03-17 23:56:11 +0200

    added-notification-and-reset-btn

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Description from "./components/Description/Description.jsx";
 import Options from "./components/Options/Options.jsx";
 import Feedback from "./components/Feedback/Feedback.jsx";
+import Notification from "./components/Notification/Notification.jsx";
 
 function App() {
   const [initialFeedback, setInitialFeedback] = useState({
@@ -10,18 +11,38 @@ function App() {
     bad: 0,
   });
 
+  const [feedbacksVisible, setFeedbacksVisible] = useState(false);
+
+  const feedbacksTotal =
+    initialFeedback.good + initialFeedback.neutral + initialFeedback.bad;
+
   const updateFeedback = (feedbackType) => {
     setInitialFeedback({
       ...initialFeedback,
       [feedbackType]: initialFeedback[feedbackType] + 1,
     });
+    setFeedbacksVisible(true);
+  };
+
+  const resetFeedback = () => {
+    setInitialFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+    setFeedbacksVisible(false);
   };
 
   return (
     <>
       <Description />
-      <Options handleFeedback={updateFeedback} />
-      <Feedback initialFeedback={initialFeedback} />
+      <Options
+        handleFeedback={updateFeedback}
+        handleClick={resetFeedback}
+        feedbacksTotal={feedbacksTotal}
+      />
+      {feedbacksVisible && <Feedback initialFeedback={initialFeedback} />}
+      {!feedbacksVisible && feedbacksTotal === 0 && <Notification />}
     </>
   );
 }
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,9 +1,12 @@
-const Options = ({ handleFeedback }) => {
+const Options = ({ handleFeedback, handleClick, feedbacksTotal }) => {
   return (
     <div>
       <button onClick={() => handleFeedback("good")}>Good</button>
       <button onClick={() => handleFeedback("neutral")}>Neutral</button>
       <button onClick={() => handleFeedback("bad")}>Bad</button>
+      {feedbacksTotal > 0 && (
+        <button onClick={() => handleClick()}>Reset</button>
+      )}
     </div>
   );
 };
